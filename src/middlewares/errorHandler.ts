97380b676a879b6283eef139d17330d8d89a396b
Alex Bessa <alexbessa.dev@gmail.com>
2026-07-28 22:12:57 -0300

    feat: adicionar captura de erro para ma requisicao

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,13 +1,13 @@
 import type { NextFunction, Request, Response } from 'express';
-import { NotFoundError } from '../errors/index.ts';
+import { NotFoundError, ValidationError } from '../errors/index.ts';
 
 export default function errorHandler(
 	error: unknown,
 	_request: Request,
 	response: Response,
 	_next: NextFunction
-) {
-	if (error instanceof NotFoundError) {
+): void {
+	if (error instanceof NotFoundError || error instanceof ValidationError) {
 		response.status(error.statusCode).json({ message: error.message });
 		return;
 	}
