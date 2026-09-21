20b17c1959913174dde704117f9c6e9b776293d5
baezacontato <baezacontato@gmail.com>
2026-09-21 23:50:55 +0000

    feat: finalizar crud de faturas

--- src/controllers/invoice.controller.ts
+++ src/controllers/invoice.controller.ts
@@ -1,4 +1,9 @@
 import type { Request, Response } from 'express';
+import type { InvoiceStatus } from '../../prisma/generated/client.ts';
+import type {
+	CreateInvoice,
+	UpdateInvoice
+} from '../schemas/invoice.schema.ts';
 import * as InvoiceService from '../services/invoice.service.ts';
 
 export async function getAllInvoices(
@@ -10,4 +15,58 @@ export async function getAllInvoices(
 	const invoices = await InvoiceService.findAllInvoices(page);
 
 	response.status(200).json(invoices);
+}
+
+export async function getInvoiceById(
+	request: Request,
+	response: Response
+): Promise<void> {
+	const id = Number(request.params.id);
+
+	const invoice = await InvoiceService.findInvoiceById(id);
+
+	response.status(200).json(invoice);
+}
+
+export async function createInvoice(
+	request: Request,
+	response: Response
+): Promise<void> {
+	const { customerId, amount, status, date } =
+		request.body as CreateInvoice & { customerId: number };
+
+	const invoice = await InvoiceService.insertInvoice(customerId, {
+		amount,
+		status,
+		date
+	});
+
+	response.status(201).json(invoice);
+}
+
+export async function updateInvoice(
+	request: Request,
+	response: Response
+): Promise<void> {
+	const id = Number(request.params.id);
+	const { amount, status, date } = request.body as UpdateInvoice;
+
+	const invoice = await InvoiceService.modifyInvoice(id, {
+		amount,
+		status,
+		date
+	});
+
+	response.status(200).json(invoice);
+}
+
+export async function deleteInvoice(
+	request: Request,
+	response: Response
+): Promise<void> {
+	const id = Number(request.params.id);
+
+	await InvoiceService.removeInvoice(id);
+
+	response.status(204).send();
 }
